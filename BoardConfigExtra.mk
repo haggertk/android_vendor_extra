d212fc3390fcf5d0afacb503db5cde9b72024fcf
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-10 16:25:24 -0700

    Update TWRP flags
    * Update flag to exclude TWRP app from recovery
    * Include NTFS-3G
    
    Change-Id: I266d112b96031ebe374ef164bdb68d615edb8f27

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -3,6 +3,7 @@ TARGET_USE_SDCLANG := true
 
 # TWRP
 TW_DEVICE_VERSION ?= $(shell date -u +"%Y%m%d_%H%M")-kfh
-TW_EXCLUDE_APP := true
 TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
+TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := false
