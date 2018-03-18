e3099f248286e8716a8ddb24b76cccfaffafa95a
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-18 15:07:50 -0600

    Copy adb public keys to recovery
    Change-Id: I7c071368fcde3fdb44ee50055b8d53a74cdcc43d

--- product.mk
+++ product.mk
@@ -4,3 +4,7 @@ GAPPS_EMBED_ARM64_PRODUCT := lineage_mata
 ifneq ($(filter $(GAPPS_EMBED_ARM64_PRODUCT),$(TARGET_PRODUCT)),)
   -include vendor/gapps/arm64/arm64-vendor.mk
 endif
+
+# Recovery ADB keys
+PRODUCT_COPY_FILES += \
+    vendor/extra/adb_keys:recovery/root/adb_keys
