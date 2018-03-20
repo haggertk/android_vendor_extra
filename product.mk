f1ad9b761846630b9a0bde20e12edb32698b64ba
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-20 06:38:35 -0600

    Unconditionally include gapps vendor.mk
    * It's a bad thing if you lose your gapps on flash, so let the
      build error hard if the repo disappears for some reason
    
    Change-Id: I4645e11813b1f37ec9b57234a239296ceb456123

--- product.mk
+++ product.mk
@@ -2,7 +2,7 @@
 GAPPS_EMBED_ARM64_PRODUCT := lineage_mata
 
 ifneq ($(filter $(GAPPS_EMBED_ARM64_PRODUCT),$(TARGET_PRODUCT)),)
-  -include vendor/gapps/arm64/arm64-vendor.mk
+  include vendor/gapps/arm64/arm64-vendor.mk
 endif
 
 # Recovery ADB keys
